6b5501a8d9b25fb4018f00289da84953802aa188
Maike <maikonalexandre574@gmail.com>
2024-11-26 10:39:13 -0300

    feat: add yup validation

--- src/pages/auth/form.jsx
+++ src/pages/auth/form.jsx
@@ -16,6 +16,15 @@ import {
   Flex,
 } from "@chakra-ui/react";
 
+import * as Yup from "yup";
+
+const schema = Yup.object({
+  email: Yup.string().email("Email inválido!").required("Email obrigatório!"),
+  password: Yup.string()
+    .min(6, "Email precisa ter no mínimo 6 caracteres!")
+    .required("Senha obrigatória!"),
+});
+
 export function Form() {
   const formik = useFormik({
     initialValues: {
@@ -25,7 +34,8 @@ export function Form() {
     onSubmit: (values) => {
       console.log("Values", values);
     },
-    // validationSchema:
+    validateOnChange: false,
+    validationSchema: schema,
   });
 
   return (
@@ -35,12 +45,17 @@ export function Form() {
           <Text color="orange.600">Email</Text>
           <Input
             w="xs"
-            type="email"
+            type="text"
             name="email"
             placeholder="exemplo@exemplo.com"
             onChange={formik.handleChange}
             value={formik.values.email}
           />
+          {formik.errors.email && (
+            <Text color="red.500" fontSize="sm">
+              {formik.errors.email}
+            </Text>
+          )}
         </Box>
         <Box>
           <Text color="orange.600">Senha</Text>
@@ -51,22 +66,15 @@ export function Form() {
             onChange={formik.handleChange}
             value={formik.values.password}
           />
+          {formik.errors.password && (
+            <Text color="red.500" fontSize="sm">
+              {formik.errors.password}
+            </Text>
+          )}
         </Box>
         <Button mt="8" bg="orange.400" w="xs" type="submit">
           Login
         </Button>
-        {/* <Flex w="full" justifyContent="space-between">
-          <Box>
-            <Text fontWeight="medium" color="orange.600">
-              Esqueci minha senha
-            </Text>
-          </Box>
-          <Box>
-            <Text fontSize="sm" fontWeight="medium" color="orange.600">
-              Cadastrar
-            </Text>
-          </Box>
-        </Flex> */}
       </VStack>
     </form>
   );
