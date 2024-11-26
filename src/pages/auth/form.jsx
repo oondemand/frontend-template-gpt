a113f7847485d48a2d1a756922391835e285a6b2
Maike <maikonalexandre574@gmail.com>
2024-11-26 10:22:27 -0300

    feat: create login form

--- src/pages/auth/form.jsx
+++ src/pages/auth/form.jsx
@@ -1,7 +1,73 @@
+import {
+  Formik,
+  Field,
+  ErrorMessage,
+  Form as FormikForm,
+  useFormik,
+} from "formik";
+
+import {
+  Button,
+  Input,
+  VStack,
+  Text,
+  Box,
+  HStack,
+  Flex,
+} from "@chakra-ui/react";
+
 export function Form() {
+  const formik = useFormik({
+    initialValues: {
+      email: "",
+      password: "",
+    },
+    onSubmit: (values) => {
+      console.log("Values", values);
+    },
+    // validationSchema:
+  });
+
   return (
-    <div>
-      <p>form</p>
-    </div>
+    <form onSubmit={formik.handleSubmit} onReset={formik.handleReset}>
+      <VStack gap="2">
+        <Box>
+          <Text color="orange.600">Email</Text>
+          <Input
+            w="xs"
+            type="email"
+            name="email"
+            placeholder="exemplo@exemplo.com"
+            onChange={formik.handleChange}
+            value={formik.values.email}
+          />
+        </Box>
+        <Box>
+          <Text color="orange.600">Senha</Text>
+          <Input
+            w="xs"
+            type="password"
+            name="password"
+            onChange={formik.handleChange}
+            value={formik.values.password}
+          />
+        </Box>
+        <Button mt="8" bg="orange.400" w="xs" type="submit">
+          Login
+        </Button>
+        {/* <Flex w="full" justifyContent="space-between">
+          <Box>
+            <Text fontWeight="medium" color="orange.600">
+              Esqueci minha senha
+            </Text>
+          </Box>
+          <Box>
+            <Text fontSize="sm" fontWeight="medium" color="orange.600">
+              Cadastrar
+            </Text>
+          </Box>
+        </Flex> */}
+      </VStack>
+    </form>
   );
 }
--- src/pages/auth/index.jsx
+++ src/pages/auth/index.jsx
@@ -1,4 +1,5 @@
 import { Flex, Box, Heading, HStack, VStack, Text } from "@chakra-ui/react";
+import { Formik, Field, ErrorMessage } from "formik";
 import { Form } from "./form";
 
 export function Auth() {
@@ -19,12 +20,14 @@ export function Auth() {
         <Flex alignItems="center" px="8" h="96" bg="orange.400">
           <Heading color="white">Poc Template Gpt</Heading>
         </Flex>
-        <VStack px="8">
-          <Heading color="orange.600">Que bom ter você por aqui!:</Heading>
-          <Text color="gray.700">
-            Vamos juntos transformar sua rotina com tecnologia.
-          </Text>
-          <Form></Form>
+        <VStack px="8" gap="8">
+          <VStack>
+            <Heading color="orange.600">Que bom ter você por aqui!:</Heading>
+            <Text color="gray.700">
+              Vamos juntos transformar sua rotina com tecnologia.
+            </Text>
+          </VStack>
+          <Form />
         </VStack>
       </Flex>
     </Flex>
