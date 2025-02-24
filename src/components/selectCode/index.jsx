b3514ead6c61a0b5558bdf589afb29817832378e
Maike <maikonalexandre574@gmail.com>
2025-02-24 09:57:39 -0300

    fix: refactor SelectCode component to improve state management and simplify props

--- src/components/selectCode/index.jsx
+++ src/components/selectCode/index.jsx
@@ -1,41 +1,32 @@
 import { Box, Text, VStack, List, Input } from "@chakra-ui/react";
 import { useEffect, useState, useRef } from "react";
 
-export const SelectCode = ({
-  data,
-  label = "Codigo",
-  w = "sm",
-  labelStyles,
-  defaultValue,
-  onBlur,
-  ...rest
-}) => {
+export const SelectCode = ({ data, onChange, value, w = "sm" }) => {
   const [closeList, setCloseList] = useState(true);
   const [options, setOptions] = useState(data);
-  const [selectedValue, setSelectedValue] = useState(defaultValue ?? "");
 
   const listRef = useRef(null);
   const inputRef = useRef(null);
 
   const handleInputChange = (value) => {
-    setSelectedValue(value);
+    onChange({ value });
   };
 
   const handleOptionClick = (value) => {
-    setSelectedValue(value);
     setCloseList(true);
+    onChange({ value });
   };
 
   useEffect(() => {
     const timeout = setTimeout(() => {
       const result = data.filter((e) =>
-        e?.toLowerCase()?.trim()?.includes(selectedValue?.toLowerCase()?.trim())
+        e.toLowerCase().trim().includes(value.toLowerCase().trim())
       );
       setOptions(result);
     }, 500);
 
     return () => clearTimeout(timeout);
-  }, [selectedValue]);
+  }, [value]);
 
   const handleClickOutside = (event) => {
     if (
@@ -56,26 +47,19 @@ export const SelectCode = ({
   }, []);
 
   return (
-    // <form onBlur={onBlur} defaultValue={defaultValue}>
     <Box position="relative" mt="1" w={w}>
-      <Box align="start" mb="1" gap="0">
-        {label && (
-          <Text color="orange.600" {...labelStyles}>
-            {label}
-          </Text>
-        )}
+      <VStack align="start" mb="2" gap="1">
+        <Text color="orange.600">Codigo</Text>
         <Input
           ref={inputRef}
           w="full"
           placeholder=""
-          value={selectedValue}
+          value={value}
           onChange={(e) => handleInputChange(e.target.value)}
-          autoComplete="off"
+          autoComplete="email"
           onFocus={() => setCloseList(false)}
-          {...rest}
-          onBlur={onBlur}
         />
-      </Box>
+      </VStack>
       {!closeList && options.length > 0 && (
         <List.Root
           ref={listRef}
@@ -86,26 +70,29 @@ export const SelectCode = ({
           border="1px solid #ddd"
           borderRadius="md"
           boxShadow="sm"
-          zIndex="999"
+          zIndex="10"
           maxHeight="300px"
           overflowY="auto"
           listStyleType="none"
           bg="white"
         >
-          {options.map((element, index) => (
-            <List.Item
-              p={2}
-              key={`${element} + ${index}`}
-              cursor="pointer"
-              _hover={{ bg: "gray.100" }}
-              onClick={() => handleOptionClick(element)}
-            >
-              {element}
-            </List.Item>
-          ))}
+          {options.map((element, index) => {
+            return (
+              <List.Item
+                p={2}
+                key={`${element} + ${index}`}
+                cursor="pointer"
+                _hover={{ bg: "gray.100" }}
+                onClick={() => {
+                  handleOptionClick(element);
+                }}
+              >
+                {element}
+              </List.Item>
+            );
+          })}
         </List.Root>
       )}
     </Box>
-    // </form>
   );
 };
