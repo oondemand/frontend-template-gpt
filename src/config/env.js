b0ccb6c14f7e4690f2e72fe7cc6de02b5be9bf61
Maike <maikonalexandre574@gmail.com>
2025-02-26 10:29:40 -0300

    feat: add SERVICE_VERSION to environment configuration and display it in the Login component

--- src/config/env.js
+++ src/config/env.js
@@ -4,6 +4,7 @@ const envSchema = Yup.object({
   MODE: Yup.string().default("development"),
   VITE_API_URL: Yup.string().required(),
   VITE_API_INTEGRACAO_URL: Yup.string().required(),
+  SERVICE_VERSION: Yup.string().default("1.0.0"),
 });
 
 export const env = envSchema.validateSync(import.meta.env);
